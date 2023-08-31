a2d5887c814e52a663c0147d3b5b72591fd8630e
dodgog <137945767+dodgog@users.noreply.github.com>
2023-08-30 17:36:17 -0700

    Zoom strokes and draw only outline

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:perfect_freehand/perfect_freehand.dart';
 
@@ -22,6 +26,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final List<double> ratios = [];
+
 // Home of the project
 class _TouchTracer extends StatefulWidget {
   const _TouchTracer();
@@ -35,9 +41,9 @@ class DefaultDrawStrokeProperties {
   static const bool processAndDrawPressure = true;
 
   /// Parameters for the perfect_freehand stroke processing
-  static const double size = 3;
-  static const double thinning = 0.2;
-  static const double smoothing = 0;
+  static const double size = 1;
+  static const double thinning = 0; //0.2;
+  static const double smoothing = 0.8; //0;
   static const double streamline = 0.5;
   static const double taperStart = 0;
   static const double taperEnd = 0;
@@ -74,6 +80,8 @@ class _TouchTracerState extends State<_TouchTracer> {
     if (currentStroke == null) {
       return;
     }
+    // If [processAndDrawPressure] is set to true, pressure value is
+    // going to be either the reported physical pressure or 1.0
     _currentStroke.value = currentStroke.copyWithPoint(
         event.localPosition, DefaultDrawStrokeProperties.processAndDrawPressure ? event.pressure : null);
   }
@@ -84,6 +92,11 @@ class _TouchTracerState extends State<_TouchTracer> {
       return;
     }
     _pastStrokes.value.add(_processStroke(endedStroke)!);
+
+    ratios.add(_processStroke(endedStroke)!.length / endedStroke.points.length);
+    //print("${_processStroke(endedStroke)!.length} / ${endedStroke.points.length}");
+    //print(ratios.reduce((a, b) => a + b) / ratios.length);
+
     // set copy to trigger re-draw
     _pastStrokes.value = _pastStrokes.value.toList();
     _currentStroke.value = null;
@@ -93,7 +106,11 @@ class _TouchTracerState extends State<_TouchTracer> {
     _pastStrokes.value = [];
   }
 
-  void _onRedrawPastStrokes() {
+  void _onUndoLastStroke() {
+    // Run twice because the press of the button is a stroke
+    // TODO fix
+    _pastStrokes.value.removeLast();
+    _pastStrokes.value.removeLast();
     _pastStrokes.value = _pastStrokes.value.toList();
   }
 
@@ -122,7 +139,7 @@ class _TouchTracerState extends State<_TouchTracer> {
                 child: Row(
                   children: [
                     IconButton(onPressed: _onClearStrokes, icon: const Icon(Icons.ac_unit_outlined)),
-                    IconButton(onPressed: _onRedrawPastStrokes, icon: const Icon(Icons.redo)),
+                    IconButton(onPressed: _onUndoLastStroke, icon: const Icon(Icons.undo)),
                   ],
                 ),
               ),
@@ -137,13 +154,16 @@ class _TouchTracerState extends State<_TouchTracer> {
 class _Stroke {
   const _Stroke(this.points, [this.pressures]);
   final List<Offset> points;
+  // This list won't be initialized if [processAndDrawPressure] is false.
   final List<double>? pressures;
-  //final List<Point>? canvasPoints;
 
   // Add a point to the copy of list of points and return this new extended copy
   _Stroke copyWithPoint(Offset point, [double? pressure]) {
     List<double>? newPressures;
     final newPoints = points.toList();
+    // If pressures are collected, [processAndDrawPressure] is set true, the list
+    // of pressures is not null and the pressure value is not null.
+    // The list [pressures] is then always the same length as the list [points].
     if (pressures != null && pressure != null) {
       newPressures = pressures?.toList();
       newPressures?.add(pressure);
@@ -170,8 +190,8 @@ class _PastStrokePainter extends CustomPainter {
     }
 
     for (var pointList in pointLists) {
-      _drawPointsFromList(canvas, pointList);
-      //_drawStroke(canvas, stroke);
+      //_drawPointsFromList(canvas, pointList);
+      _centeredDrawPointsFromList(canvas, pointList, size);
     }
   }
 }
@@ -201,7 +221,7 @@ List<Point>? _processStroke(_Stroke stroke) {
     return null;
   }
 
-  final List<Point> pointsList = [];
+  final List<Point> pointsList = List.filled(stroke.points.length, const Point(0, 0), growable: false);
   for (var i = 0; i <= stroke.points.length - 1; i++) {
     late final Point point;
     if (stroke.pressures != null) {
@@ -209,8 +229,10 @@ List<Point>? _processStroke(_Stroke stroke) {
     } else {
       point = Point(stroke.points[i].dx, stroke.points[i].dy);
     }
-    pointsList.add(point);
+    pointsList[i] = point;
   }
+
+  //final points = stroke.points.map((e, i) => Point(e.dx, e.dy, ))
 
   final outlinePoints = getStroke(
     pointsList,
@@ -224,6 +246,7 @@ List<Point>? _processStroke(_Stroke stroke) {
     capEnd: DefaultDrawStrokeProperties.capEnd,
     simulatePressure: DefaultDrawStrokeProperties.simulatePressure,
   );
+  //print("${pointsList.length} -> ${outlinePoints.length}");
 
   return outlinePoints;
 }
@@ -246,7 +269,7 @@ void _drawPointsFromList(Canvas canvas, List<Point> pointsList) {
         path.quadraticBezierTo(p0.x, p0.y, ((p0.x + p1.x) / 2), ((p0.y + p1.y) / 2));
         break;
       case 3:
-        if (i >= pointsList.length - 1) break;
+        if (i >= pointsList.length - 2) break;
         final p1 = pointsList[i + 1];
         final p2 = pointsList[i + 2];
         path.cubicTo(p0.x, p0.y, p1.x, p1.y, p2.x, p2.y);
@@ -254,9 +277,118 @@ void _drawPointsFromList(Canvas canvas, List<Point> pointsList) {
       default:
         break;
     }
+    //path.close();
+    path.fillType = PathFillType.nonZero;
   }
 
-  canvas.drawPath(path, Paint()..color = DefaultDrawStrokeProperties.color);
+  canvas.drawPath(
+      path,
+      Paint()
+        ..color = DefaultDrawStrokeProperties.color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 1);
+}
+
+void _centeredDrawPointsFromList(Canvas canvas, List<Point> pointsList, Size size) {
+  Path path = Path();
+
+  double minX = double.infinity;
+  double minY = double.infinity;
+  double maxX = double.negativeInfinity;
+  double maxY = double.negativeInfinity;
+
+  for (int i = 1; i < pointsList.length - 1; i++) {
+    final p0 = pointsList[i];
+
+    if (p0.x < minX) minX = p0.x;
+    if (p0.x > maxX) maxX = p0.x;
+    if (p0.y < minY) minY = p0.y;
+    if (p0.y > maxY) maxY = p0.y;
+  }
+
+  double frameThickness = 3;
+  /*
+  maxX += frameThickness;
+  minX -= frameThickness;
+  maxY += frameThickness;
+  minY -= frameThickness;
+  */
+
+  double height = maxY - minY + 2 * frameThickness;
+  double width = maxX - minX + 2 * frameThickness;
+
+  Offset firstPoint = Offset(pointsList[0].x, pointsList[0].y);
+  Offset initialShift = Offset(-minX + frameThickness, -minY + frameThickness);
+  double scaleVertical = height / (size.height);
+  double scaleHorizontal = width / (size.width);
+  double scale = max(scaleHorizontal, scaleVertical);
+
+  Offset p0 = (firstPoint + initialShift) / scale;
+  print(p0);
+  path.moveTo(p0.dx, p0.dy);
+
+  final gradient = LinearGradient(
+    colors: generateColors(pointsList.length),
+  );
+  for (int i = 1; i < pointsList.length - 1; i++) {
+    print("$i $p0");
+    Offset p0last = p0;
+    p0 = Offset(pointsList[i].x, pointsList[i].y);
+    p0 = (p0 + initialShift) / scale;
+    path.lineTo(p0.dx, p0.dy);
+    //canvas.draw
+
+    canvas.drawLine(p0last, p0, Paint()..color = gradient.colors[i]);
+    /*
+    canvas.drawPath(
+        path,
+        Paint()
+          ..color = DefaultDrawStrokeProperties.color
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 1);
+    */
+
+    //path.fillType = PathFillType.nonZero;
+  }
+
+/*
+  canvas.drawPath(
+      path,
+      Paint()
+        ..color = DefaultDrawStrokeProperties.color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 1);
+        */
+
+  canvas.drawRect(
+      Rect.fromPoints((Offset(minX, minY) + initialShift) / scale, (Offset(maxX, maxY) + initialShift) / scale),
+      Paint()
+        ..color = Colors.black
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 1);
+
+  /*
+  Vertices(
+    VertexMode mode,
+    List<Offset> positions,
+    {List<Color>? colors,
+    List<Offset>? textureCoordinates,
+    List<int>? indices}
+  )
+  */
+  //Vertices vertices = Vertices(VertexMode.triangles,  )
+
+  //canvas.drawVertices(vertices, BlendMode.srcOver, Paint()..color = Colors.blue);
+}
+
+List<Color> generateColors(int numberOfColors) {
+  final Color startColor = Colors.red;
+  final Color endColor = Colors.blue;
+
+  return List.generate(numberOfColors, (index) {
+    final t = index / (numberOfColors - 1);
+    return Color.lerp(startColor, endColor, t)!;
+  });
 }
 
 void _drawStroke(Canvas canvas, _Stroke stroke) {
